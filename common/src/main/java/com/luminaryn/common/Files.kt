a6ed3298a9a057dd0f408e3a9fc7141247056647
Tim Totten <tim@totten.ca>
2021-07-19 06:30:16 -0700

    A couple minor tweaks.

--- common/src/main/java/com/luminaryn/common/Files.kt
+++ common/src/main/java/com/luminaryn/common/Files.kt
@@ -1,10 +1,27 @@
 package com.luminaryn.common
 
 import java.io.File
+import java.util.*
 
 object Files {
     @JvmStatic
     fun getExtension(filename: String): String {
         return File(filename).extension;
+    }
+
+    fun isImage(filename: String): Boolean {
+        val ext = getExtension(filename).uppercase(Locale.getDefault())
+        return when (ext) {
+            "JPG", "PNG", "JPEG", "WEBP", "HEIC", "GIF" -> true
+            else -> false
+        }
+    }
+
+    fun isVideo(filename: String): Boolean {
+        val ext = getExtension(filename).uppercase(Locale.getDefault())
+        return when (ext) {
+            "MP4", "MKV", "AVI", "WEBM", "M4V", "MPG", "MPEG", "MPEG4", "MOV" -> true
+            else -> false
+        }
     }
 }
